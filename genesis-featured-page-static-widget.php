a77729b9629d694cec43607a0c6c078571d6c7a6
Robin Cornett <hello@robincornett.com>
2014-09-26 09:06:20 -0400

    deactivates if Genesis Featured Page widget isn't running

--- genesis-featured-page-static-widget.php
+++ genesis-featured-page-static-widget.php
@@ -6,27 +6,44 @@
  * @license GPL-2.0+
  *
  * @wordpress-plugin
- * Plugin Name: Featured Page Widget for Genesis sites with Static Home Page
+ * Plugin Name: Featured Page Widget for Genesis Sites with Static Home Page
  * Description: Widget to Display featured page. Tweaked to allow for a site using a static home page. This replaces the built in Genesis Featured Page widget.
- * Version:     1.0.1
+ * Version:     1.0.2
  * Author:      Robin Cornett
  * Author URI:  http://robincornett.com/
  * Text Domain: genesis-featured-page-static-widget
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /languages
-*/
+ */
 
 // if this file is called directly abort
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
- // Register the widget
-add_action( 'widgets_init', 'featuredpagestatic_register_widget', 20 );
+add_action( 'widgets_init', 'featuredpagestatic_init', 10 );
+function featuredpagestatic_init() {
+	if ( ! class_exists( 'Genesis_Featured_Page' ) ) {
+		add_action( 'admin_init', 'featuredpagestatic_deactivate' );
+		add_action( 'admin_notices', 'featuredpagestatic_notice' );
+		return;
+	}
+
+}
+
+// Register the widget
+add_action( 'widgets_init', 'featuredpagestatic_register_widget', 15 );
 function featuredpagestatic_register_widget() {
 	unregister_widget( 'Genesis_Featured_Page' );
 	register_widget( 'Genesis_Featured_Page_Static_Widget' );
+}
+
+function featuredpagestatic_deactivate() {
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+}
+
+function featuredpagestatic_notice() {
+	echo '<div class="error"><p><strong>Featured Page Widget for Genesis Sites with Static Home Page</strong> works only with the Genesis Framework. It has been <strong>deactivated</strong>.</p></div>';
 }
 
 require plugin_dir_path( __FILE__ ) . 'includes/class-genesisfeaturedpagestaticwidget.php';
